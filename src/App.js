d45f70f8eea6c24ff23ca833b2018a21b5425f7c
Abdul-Mutaleb <abdulmutaleb528@gmail.com>
2024-08-22 15:16:27 +0600

    add to others components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Courses from './Components/Courses/Courses';
+import ContactUs from './Components/ContactUs/ContactUs';   
 import NavbarExample from './Components/NavbarExample/NavbarExample';
 
 function App() {
   return (
     <Router>
-    <>
-     <NavbarExample/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </>
-  </Router>
+      <>
+        <NavbarExample />
+        <Routes>
+        <Route path='/*' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/contact' element={<ContactUs />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
--- src/Components/NavbarExample/NavbarExample.js
+++ src/Components/NavbarExample/NavbarExample.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Logo from '../../images/logo.png';
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -10,7 +10,7 @@ import './NavbarExample.css';
 const NavbarExample = () => {
     const [expanded, setExpanded] = useState(false); 
     return (
-        <Navbar expand="lg" className="bg-body-light nav-style">
+        <Navbar expand="lg" className="bg-body-light nav-style" expanded={expanded}>
           <Container fluid>
             <Navbar.Brand as={Link} to="/" style={{fontSize:'30px'}}>
                 <img src={Logo} alt="Online Study" style={{ width: '50px', marginRight: '10px',}} />
@@ -18,28 +18,25 @@ const NavbarExample = () => {
             </Navbar.Brand>
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} className="bg-gradient">
-                    {expanded ? (
-                        <FontAwesomeIcon icon={faTimes} className="fa icon-close" />
-                    ) : (
-                        <FontAwesomeIcon icon={faBars} className="fa icon-expand" />
-                    )}
-                </Navbar.Toggle>
+                {expanded ? (
+                    <FontAwesomeIcon icon={faTimes} className="fa icon-close" />
+                ) : (
+                    <FontAwesomeIcon icon={faBars} className="fa icon-expand" />
+                )}
+            </Navbar.Toggle>
 
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto text-body" style={{fontSize:'15px',marginRight:'20px'}}>
-
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/courses">Courses</Nav.Link>
-                    <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link> 
+                    <Nav.Link as={NavLink} to="/" exact onClick={() => setExpanded(false)}>Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about" onClick={() => setExpanded(false)}>About</Nav.Link>
+                    <Nav.Link as={NavLink} to="/courses" onClick={() => setExpanded(false)}>Courses</Nav.Link>
+                    <Nav.Link as={NavLink} to="/contact" onClick={() => setExpanded(false)}>Contact Us</Nav.Link> 
                 </Nav>
                 <Button className="button-style">Search</Button>
                 <FontAwesomeIcon icon={faMoon} className="ms-3"/>
-              
             </Navbar.Collapse>
         </Container>
     </Navbar>
-   
     );
 };
 
